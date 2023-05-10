34276f654674a020b5e09eff50fbb5700c45fecb
Shudhanshu07-coder <shudhanshu.bhumca13@gmail.com>
2023-05-10 15:24:31 +0530

    commit 1

---
+++ src/main/java/com/example/shubhamcicd/ShubhamCicdApplication.java
@@ -0,0 +1,13 @@
+package com.example.shubhamcicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShubhamCicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShubhamCicdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/shubhamcicd/ShubhamCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shubhamcicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShubhamCicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
